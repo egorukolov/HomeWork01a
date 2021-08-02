88141f4f3124b84105ad86d8ab170bd917777c5a
Egor Ukolov <ukolovegor@yandex.ru>
2021-08-02 20:22:20 +0700

    Initial Commit

---
+++ HomeWork01a/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork01a
+//
+//  Created by Egor Ukolov on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
